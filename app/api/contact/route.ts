717a20160cd6e020642b2de38199362bdf67fa6f
Sohaib Younas <sohaibyounas24@gmail.com>
2026-08-18 11:09:03 +0500

    fixed deployment issue

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,11 +1,10 @@
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
-// Initialize Resend with the API key from environment variables
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(req: Request) {
   try {
+    // Initialize Resend with the API key from environment variables
+    const resend = new Resend(process.env.RESEND_API_KEY);
     const { name, email, message } = await req.json();
 
     if (!name || !email || !message) {
